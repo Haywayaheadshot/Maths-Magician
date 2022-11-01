44b8b82d00cf91ab3085066c5cefb092d5b1ab6d
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2022-11-01 22:48:19 +0000

    Refactor App.js to be a class

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React, { Component } from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-const App = () => (
-  <div className="App">
-    <Calculator />
-  </div>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Calculator />
+      </div>
+    );
+  }
+}
 
 export default App;
