65cde683b2e855663f7fa6e68913fe31fe480190
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2022-11-01 22:32:43 +0000

    Refactor calculator.js to be a class

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,43 +1,46 @@
-import React from 'react';
+/* eslint-disable */
+import React, { Component } from 'react';
 import './calculator.css';
 
-function Calculator() {
-  return (
-    <div className="calculator-container">
-      <div className="calculator">
-        <input className="output-display" htmlFor="textarea" placeholder="0" disabled />
-        <div className="buttons-div">
-          <button className="buttons" type="button">AC</button>
-          <button className="buttons" type="button">+/-</button>
-          <button className="buttons" type="button">%</button>
-          <button className="buttons" type="button">÷</button>
-        </div>
-        <div className="buttons-div">
-          <button className="buttons" type="button">7</button>
-          <button className="buttons" type="button">8</button>
-          <button className="buttons" type="button">9</button>
-          <button className="buttons" type="button">x</button>
-        </div>
-        <div className="buttons-div">
-          <button className="buttons" type="button">4</button>
-          <button className="buttons" type="button">5</button>
-          <button className="buttons" type="button">6</button>
-          <button className="buttons" type="button">-</button>
-        </div>
-        <div className="buttons-div">
-          <button className="buttons" type="button">1</button>
-          <button className="buttons" type="button">2</button>
-          <button className="buttons" type="button">3</button>
-          <button className="buttons" type="button">+</button>
-        </div>
-        <div className="buttons-last-div">
-          <button className="buttons" type="button">0</button>
-          <button className="buttons" type="button">.</button>
-          <button className="buttons" type="button">=</button>
+class Calculator extends Component {
+  render() {
+    return (
+      <div className="calculator-container">
+        <div className="calculator">
+          <input className="output-display" htmlFor="textarea" placeholder="0" disabled />
+          <div className="buttons-div">
+            <button className="buttons" type="button">AC</button>
+            <button className="buttons" type="button">+/-</button>
+            <button className="buttons" type="button">%</button>
+            <button className="buttons" type="button">÷</button>
+          </div>
+          <div className="buttons-div">
+            <button className="buttons" type="button">7</button>
+            <button className="buttons" type="button">8</button>
+            <button className="buttons" type="button">9</button>
+            <button className="buttons" type="button">x</button>
+          </div>
+          <div className="buttons-div">
+            <button className="buttons" type="button">4</button>
+            <button className="buttons" type="button">5</button>
+            <button className="buttons" type="button">6</button>
+            <button className="buttons" type="button">-</button>
+          </div>
+          <div className="buttons-div">
+            <button className="buttons" type="button">1</button>
+            <button className="buttons" type="button">2</button>
+            <button className="buttons" type="button">3</button>
+            <button className="buttons" type="button">+</button>
+          </div>
+          <div className="buttons-last-div">
+            <button className="buttons" type="button">0</button>
+            <button className="buttons" type="button">.</button>
+            <button className="buttons" type="button">=</button>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Calculator;
