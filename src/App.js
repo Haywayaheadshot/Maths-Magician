86b726e198686536fa1b34b5141a6279ac9d61e7
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2022-11-03 16:49:47 +0000

    Refactor from class to function

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import React, { Component } from 'react';
+// import React, { useState } from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Calculator />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <Calculator />
+  </div>
+);
 
 export default App;
