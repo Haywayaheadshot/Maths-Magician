c548b6bf6ac721ef31aacbf14896c94bb8e70d10
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2022-11-09 18:52:48 +0000

    Tried to do hamburger for mobile version but failed to load ul so I put the ul directly

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,36 +1,45 @@
 import React from 'react';
 import './navigation.css';
+// import Hamburger from 'hamburger-react';
 import logo from '../images/icons/logo.png';
-import menu from '../images/icons/hamburger-icon.png';
 
-const Navigation = () => {
-  // const hamburgerIcon = document.querySelector('.hamburger-section');
-
-  const hambugerHandler = () => (
-    console.log('Clicked')
-    // const div = document.createElement('div')
-  );
-
-  return (
-    <div>
-      <nav className="navigation">
-        <section className="logo-div">
-          <img className="logo-image" src={logo} alt="Maths Magician Logo" />
-          Maths Magician
-        </section>
-        <button className="hamburger-button for-phone" type="button" onClick={() => hambugerHandler()}>
-          <img className="hamburger" src={menu} alt="Hamburger Icon" />
-        </button>
-        <section className="for-desktop">
-          <ul className="nav-desktop-ul">
-            <li><a className="nav-link" href="https://www.facebook.com/">Home</a></li>
-            <li><a className="nav-link" href="https://www.facebook.com/">Calculator</a></li>
-            <li><a className="nav-link" href="https://www.facebook.com/">Quotes</a></li>
-          </ul>
-        </section>
-      </nav>
-    </div>
-  );
-};
-
+const Navigation = () => (
+  <div>
+    <nav className="navigation">
+      <section className="logo-div">
+        <img className="logo-image" src={logo} alt="Maths Magician Logo" />
+        Maths Magician
+      </section>
+      {/* <div className="for-phone hamburger-div">
+        <Hamburger
+          color="#ff9500"
+          direction="right"
+          duration={0.8}
+          onToggle={(toggle) => {
+            if (toggle) {
+              return (
+                <div>
+                  <ul className="phone-ul for-phone">
+                    <li className="menu-pop-up-li">Home</li>
+                    <li className="menu-pop-up-li">Calculator</li>
+                    <li className="menu-pop-up-li">Quotes</li>
+                  </ul>
+                </div>
+              );
+            }
+            return null;
+          }}
+        />
+      </div> */}
+      <section>
+        <ul className="nav-ul">
+          <li><a className="nav-link" href="/">Home</a></li>
+          <li><a className="nav-link" href="/calculator">Calculator</a></li>
+          <li><a className="nav-link" href="/quotes">Quotes</a></li>
+        </ul>
+      </section>
+    </nav>
+  </div>
+  // );
+);
 export default Navigation;
