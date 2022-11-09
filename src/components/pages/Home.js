ca8e658af3d8a844f7b0189fb0707706ac3200cb
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2022-11-09 18:43:24 +0000

    Create home and quote page

---
+++ src/components/pages/Home.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './home.css';
+
+function Home() {
+  return (
+    <div>
+      <h1 className="header">Welcome To Our Page!</h1>
+      <p className="home-paragraph">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint dolor cupidatat
+        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint dolor cupidatat
+        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        <br />
+        <br />
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint dolor cupidatat
+        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint dolor cupidatat
+        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur.Duis aute irure reprehenderit
+        in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/pages/Quotes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './quotes.css';
+
+export default function Quotes() {
+  return (
+    <div className="container">
+      <p className="paragraph">
+        Mathematics is not about numbers, equations,
+        computations or algorithms: it is about
+        understanding. -William Paul Thurston
+      </p>
+    </div>
+  );
+}
