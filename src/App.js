16a99b433324ae9c503b86b8b6b63851e8e10604
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2022-11-09 10:56:47 +0000

    Add and import Navigation from navigation.js component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import Calculator from './components/Calculator';
+import Navigation from './components/Navigation';
 
 const App = () => (
   <div className="App">
+    <Navigation />
     <Calculator />
   </div>
 );
