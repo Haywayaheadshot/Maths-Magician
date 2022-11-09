1afb35c14d1c57a9e1cb8405e2e41c9a50806f41
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2022-11-09 18:47:31 +0000

    Import browser router from react dom and use to route app pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
 import Navigation from './components/Navigation';
+import Home from './components/pages/Home';
+import Quotes from './components/pages/Quotes';
 
 const App = () => (
-  <div className="App">
-    <Navigation />
-    <Calculator />
-  </div>
+  <Router>
+    <div className="App">
+      <Navigation />
+      <div className="container">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/calculator" element={<Calculator />} />
+          <Route exact path="/quotes" element={<Quotes />} />
+        </Routes>
+      </div>
+    </div>
+  </Router>
 );
 
 export default App;
